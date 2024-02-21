1210f9268e46a1a0b5cebe3fc2d566da732ce750
aslan-asilon31 <sulaslansetiawan31@gmail.com>
2024-02-21 08:10:23 -0800

    add some feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -139,3 +139,17 @@ sr.reveal(`.home__social`, { delay: 600 })
 sr.reveal(`.about__img, .contact__box`, { origin: 'left' })
 sr.reveal(`.about__data, .contact__form`, { origin: 'right' })
 sr.reveal(`.steps__card, .product__card, .questions__group, .footer`, { interval: 100 })
+
+
+
+
+
+// Photo Profile=======================================
+
+let profile = document.querySelector('.profile');
+let menu = document.querySelector('.menu');
+
+profile.onclick = function() {
+    menu.classList.toggle('active');
+}
+// End Photo Profile=======================================
